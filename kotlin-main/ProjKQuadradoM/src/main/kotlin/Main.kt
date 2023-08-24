42bec4822fe6429c4e4d60b09fee589b9eb98525
Roberto <105609512+RobertofPereira@users.noreply.github.com>
2023-08-24 09:22:52 -0300

    Delete kotlin-main directory

--- kotlin-main/ProjKQuadradoM/src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    print("Informe um número inteiro: ")
-    val inteiro : Int = readln().toInt()
-
-    val quadrado : Int = inteiro * inteiro
-    // val quadrado : Double = Math.pow(inteiro.toDouble())
-
-    println("\nO quadrado de $inteiro é = $quadrado")
-}
